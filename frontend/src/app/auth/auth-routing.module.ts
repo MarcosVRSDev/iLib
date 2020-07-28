24b62bdb2f91f3c4a09e2f141a06361928b023b5
MarcosVRSDev <48337521+MarcosVRSDev@users.noreply.github.com>
2020-07-27 23:17:19 -0300

    Pagina de login

---
+++ frontend/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,22 @@
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NbAuthComponent } from '@nebular/auth';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: NbAuthComponent,  // <---
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,  // <---
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class NgxAuthRoutingModule {
+}
---
+++ frontend/src/app/auth/login-page/login-page.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { NbCardModule, NbButtonModule, NbTabsetModule, NbListModule, NbBadgeModule, NbLayoutModule } from '@nebular/theme';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { LoginPageComponent } from './login-page.component';
+import { RouterModule } from '@angular/router';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
+@NgModule({
+  imports: [
+    NbCardModule,
+    ThemeModule,
+    NbButtonModule,
+    NbListModule,
+    NbTabsetModule,
+    NbCardModule,
+    RouterModule,
+    NbBadgeModule,
+    NbLayoutModule,
+    NbEvaIconsModule
+  ],
+  declarations: [
+    LoginPageComponent
+  ],
+})
+export class LoginPageModule { }
